8dd25e989c7036866cb8ae2825a891007886a089
Scott Winiarski <Scott1w1@hotmail.com>
2023-10-25 15:21:28 -0500

    Adjusted Spacing

--- JavaToCSharp/FileInput.cs
+++ JavaToCSharp/FileInput.cs
@@ -3,9 +3,9 @@ namespace JavaToCSharp;
 public class FileInput
 {
     // public string[] Lines;
-                                                    // StreamReader sr = new StreamReader("animals.txt");
-                                                    //
-                                                    // Lines = File.ReadAllLines("animals.txt");
+    // StreamReader sr = new StreamReader("animals.txt");
+    //
+    // Lines = File.ReadAllLines("animals.txt");
 
 
 }
